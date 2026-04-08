273f4e60818197745fbea12bf3766f6c3da7c084
TSK2302 <sayant2001@gmail.com>
2026-04-08 23:20:28 +0530

    initial

---
+++ app/main.py
@@ -0,0 +1,37 @@
+from fastapi import FastAPI, Depends, HTTPException
+from sqlalchemy.orm import Session
+from . import models, schemas, crud
+from .database import engine, Base, get_db
+
+Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+
+@app.post("/items", response_model=schemas.ItemResponse)
+def create(item: schemas.ItemCreate, db: Session = Depends(get_db)):
+    return crud.create_item(db, item)
+
+@app.get("/items")
+def read_all(db: Session = Depends(get_db)):
+    return crud.get_items(db)
+
+@app.get("/items/{item_id}")
+def read_one(item_id: int, db: Session = Depends(get_db)):
+    item = crud.get_item(db, item_id)
+    if not item:
+        raise HTTPException(status_code=404, detail="Not found")
+    return item
+
+@app.put("/items/{item_id}")
+def update(item_id: int, item: schemas.ItemCreate, db: Session = Depends(get_db)):
+    updated = crud.update_item(db, item_id, item)
+    if not updated:
+        raise HTTPException(status_code=404, detail="Not found")
+    return updated
+
+@app.delete("/items/{item_id}")
+def delete(item_id: int, db: Session = Depends(get_db)):
+    deleted = crud.delete_item(db, item_id)
+    if not deleted:
+        raise HTTPException(status_code=404, detail="Not found")
+    return {"message": "Deleted"}
